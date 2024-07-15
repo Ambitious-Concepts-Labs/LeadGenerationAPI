0985449bdb71be4444e4301e1e2835d54accf005
Dominique Hosea <dominique11h@yahoo.com>
2024-07-14 20:24:20 -0500

    feat(*): add api key validation

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
 import time
-from typing import Callable
+from typing import Callable, Dict
 
-from fastapi import APIRouter, FastAPI, Request, Response
-# from util.config import settings
+from fastapi import APIRouter, FastAPI, Request, Response, Security, HTTPException, Depends, Query
+from fastapi.security.api_key import APIKeyQuery, APIKeyCookie, APIKeyHeader, APIKey
+from util.config import settings
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
+
+from starlette.status import HTTP_403_FORBIDDEN
+from starlette.responses import RedirectResponse, JSONResponse
 
 import util.logger as logger
 from fastapi.middleware.cors import CORSMiddleware
 from router.leads import leadroute
 from router.emails import emailroute
+
+
+API_KEY = "1234567asdfgh"
+API_KEY_NAME = "access_token"
+COOKIE_DOMAIN = "localtest.me"
+
+api_key_query = APIKeyQuery(name=API_KEY_NAME, auto_error=False)
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+api_key_cookie = APIKeyCookie(name=API_KEY_NAME, auto_error=False)
+
+async def get_api_key(
+    api_key_query: str = Security(api_key_query),
+    api_key_header: str = Security(api_key_header),
+    api_key_cookie: str = Security(api_key_cookie),
+):
+
+    if api_key_query == API_KEY:
+        return api_key_query
+    elif api_key_header == API_KEY:
+        return api_key_header
+    elif api_key_cookie == API_KEY:
+        return api_key_cookie
+    else:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
+        )
 
 description = """
 LeadGeneration API helps automate generate leads. 🚀
@@ -158,11 +190,44 @@ async def add_process_time_header(request: Request, call_next):
     response.headers["X-Process-Time"] = str(process_time)
     return response
 
-# @app.get("/info")
-# async def info():
-#     return {
-#         "app_name": settings.app_name,
-#         "admin_email": settings.admin_email,
-#         "items_per_user": settings.items_per_user,
-#     }
+@app.get("/info")
+async def info():
+    return {
+        "app_name": settings.app_name,
+        "admin_email": settings.admin_email,
+        "items_per_user": settings.items_per_user,
+    }
 
+@app.get("/logout")
+async def route_logout_and_remove_cookie():
+    response = RedirectResponse(url="/")
+    response.delete_cookie(API_KEY_NAME, domain=COOKIE_DOMAIN)
+    return response
+
+@app.get("/")
+async def homepage():
+    return "Welcome to the security test!"
+
+@app.get("/openapi.json", tags=["documentation"])
+async def get_open_api_endpoint(api_key: APIKey = Depends(get_api_key)):
+    response = JSONResponse(
+        get_openapi(title="FastAPI security test", version=1, routes=app.routes)
+    )
+    return response
+
+@app.get("/documentation", tags=["documentation"])
+async def get_documentation(api_key: APIKey = Depends(get_api_key)):
+    response = get_swagger_ui_html(openapi_url="/openapi.json", title="docs")
+    response.set_cookie(
+        API_KEY_NAME,
+        value=api_key,
+        domain=COOKIE_DOMAIN,
+        httponly=True,
+        max_age=1800,
+        expires=1800,
+    )
+    return response
+@app.get("/secure_endpoint", tags=["test"])
+async def get_open_api_endpoint(api_key: APIKey = Depends(get_api_key)):
+    response = "How cool is this?"
+    return response
